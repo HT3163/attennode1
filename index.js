609eee136e216f62e07e6fd49c8809bec3c9a50b
Hamza tahir <hamzagujjar3163@gmail.com>
2024-05-31 00:34:40 +0500

    add mongodb url web1

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000
 dotenv.config();
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}));
 
 mongoose
     .connect(process.env.MONGO_URL, {
